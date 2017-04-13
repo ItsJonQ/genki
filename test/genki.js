18d360cbd281e12b26e87aee8ec2b08c0baa4e5c
ItsJonQ <itsjonq@gmail.com>
2017-04-12 20:57:58 -0400

    Update test + add travis

--- test/genki.js
+++ test/genki.js
@@ -3,15 +3,21 @@
 var expect = require('chai').expect;
 var genki = require('../index');
 
-describe('basic', function() {
-  it('should do a thing', function() {
-    var world = genki.start();
+describe('genki', function() {
+  var world = genki.start();
 
+  it('should inject DOM elements with jQuery', function() {
     world.$('body').html(`
       <div class="box" style="height: 10px;">Hello</div>
     `);
-
     var $box = world.$('.box');
+
+    expect($box).to.exist;
+  });
+
+  it('should get computed CSS properties', function() {
+    var $box = world.$('.box');
+
     expect($box.css('height')).to.equal('10px');
   });
 });
